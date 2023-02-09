2fa8b871c69e55fbe1f4f969ab8e109fbc35b75d
Erka <erkaibragimov@gmail.com>
2023-02-09 22:24:15 +0500

    finish models

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const PORT = process.env.PORT || 7000;
 const sequelize = require("./db.js");
+const models = require("./models/model.js");
 const app = express();
 
-app.use(bodyParser.json());
 app.use(cors());
+app.use(bodyParser.json());
 
 const start = async () => {
   try {
